b13cbacd40ee4498cfc59ff387c2f3f2f70bef4f
Ezradesu <ezraejer@gmail.com>
2023-09-09 09:30:55 +0700

    first commit

---
+++ apparant.py
@@ -0,0 +1,8 @@
+nama = "ezra"
+umur = "20"
+
+print ("perkenalkan nama saya " + nama )
+print ( nama + " adalah seorang pelajar burumur " + umur)
+
+nama = "adit"
+print ("di umur yang ke " + umur + ", " + nama + "sudah dapat belajar berjalan")
